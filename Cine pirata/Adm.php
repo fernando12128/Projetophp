27c696e9f1e6877d33a24f0c221e956e36e3c4a5
fernando12128 <fernando.augusto12128@gmail.com>
2023-06-13 22:11:56 -0300

    Atualizações

--- Cine pirata/Adm.php
+++ Cine pirata/Adm.php
@@ -4,12 +4,14 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@1,300&display=swap" rel="stylesheet">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://getbootstrap.com/docs/5.3/assets/css/docs.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
     <link rel ="stylesheet" href="css/estilo.css">
-    <link rel ="stylesheet" href="css/estilo2.css">
     <title>Adm</title>
 
 </head>
@@ -17,14 +19,21 @@
 
 <body>
     
-<div class="Nome text-center">
+<div class="Nome text-center Logo" >
         <h1>Cine Pirata</h1>
 </div>
 
-<Style>
+<div>
+     <form action="guardar-imagem.php" enctype="multipart/form-data" method="post"> 
+        
 
-</Style>
+        <input id = "arqimg" type='file' name="arqimage"  class='form-control-file' />
+        <label for="arqimg">Coloque sua Imagem</label>
 
+        <input type="submit" value="Salvar"/>
+     </form>    
+</div>
+<br>
 
 <div class="card color1">
 <section>
---
+++ Cine pirata/guardar-imagem.php
@@ -0,0 +1,38 @@
+
+<?php 
+
+if(isset($_FILES["arqimage"])){
+    $arqINome = $_FILES["arqimage"]["name"];
+    $arqITipo = $_FILES["arqimage"]["type"];
+    $arqITamanho = $_FILES["arqimage"]["size"];
+    $arqINOMETemp = $_FILES["arqimage"]["tmp_name"];
+    $erroImgMarc = $_FILES["arqimage"]["error"];
+
+    if($erroImgMarc==0){
+        if(is_uploaded_file($arqINOMETemp)){
+            if(move_uploaded_file($arqINOMETemp,"img/".$arqINome)){
+                $caminhoI = $arqINome;
+                header("location:Adm.php");
+            }
+            else{
+                $erro = "Falha ao mover imagem do marcados";
+                echo"$erro";
+            }
+        }
+        else{
+            $erro= "Erro no envio: A imagem do marcador não foi recebida com sucesso.";
+            echo "$erro";
+        }
+    }
+    else{
+        $erro = "Erro no envio: " . $erro;
+        echo "$erro";
+    }
+}
+else{
+    $erro = "Imagem do Marcador enviado não encontrado";
+    echo "$erro";
+}
+
+
+?>
